004d000519a5595766c141ac7fdc84c1ee965cca
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:35:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,11 +11,8 @@ end
 
 def display_board(board) 
   puts "  "
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+  puts rows[0]
+  puts rows[1]
   puts "  "
 end
 
