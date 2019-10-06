e84147b2768fe75c43bc3ad0424c998540283e11
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:58:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -18,11 +18,11 @@ def display_board(board)
   #rows[3] = "-----------"
   #rows[4] = " #{board[6]} | #{board[7]} | #{board[8]}"
   
-  rows = [" #{board[0]} | #{board[1]} | #{board[2]}",
+  rows = [" #{board[0]} | #{board[1]} | #{board[2]} ",
           "-----------",
-          " #{board[3]} | #{board[4]} | #{board[5]}",
+          " #{board[3]} | #{board[4]} | #{board[5]} ",
           "-----------",
-          " #{board[6]} | #{board[7]} | #{board[8]}"]
+          " #{board[6]} | #{board[7]} | #{board[8]} "]
   
   puts rows[0]
   puts rows[1]
