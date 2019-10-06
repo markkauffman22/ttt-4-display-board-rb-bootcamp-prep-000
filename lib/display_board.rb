d058a9b6c8773e938ca1093c5ac8a14462c4caf7
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:07:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -19,6 +19,9 @@ end
 
 board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
 display_board(board)
+
+
+
 # Make sure to be interpolating data from the board array within your display_board method. 
 # This is essential to solving this lab, to evolve from a permanently empty skeleton 
 # board as you did before to an actual board that contains live player movement throughout 
