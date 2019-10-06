efb1eeab35d0ca4e47ed21413fad4ee59494990e
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:44:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,14 +13,15 @@ end
 
 board = [" "," "," "," "," "," "," "," "," "]
 
-rows = []
-rows[0] = " #{board[0]} | #{board[1]} | #{board[2]}"
-rows[1] = "-----------"
-rows[2] = " #{board[3]} | #{board[4]} | #{board[5]}"
-rows[3] = "-----------"
-rows[4] = " #{board[6]} | #{board[7]} | #{board[8]}"
+
 
 def display_board(board) 
+  rows = []
+  rows[0] = " #{board[0]} | #{board[1]} | #{board[2]}"
+  rows[1] = "-----------"
+  rows[2] = " #{board[3]} | #{board[4]} | #{board[5]}"
+  rows[3] = "-----------"
+  rows[4] = " #{board[6]} | #{board[7]} | #{board[8]}"
   puts "  "
   puts rows[0]
   puts rows[1]
