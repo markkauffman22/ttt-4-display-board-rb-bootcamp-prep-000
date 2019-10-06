3b6cd26f7a4c332b64279cb61c03aff675cb9a39
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:13:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,11 +10,11 @@ def display_blank_board()
 end
 
 def display_board(board) 
-  puts " #{board[0]}  | #{board[1]}  | #{board[2]}   "
+  puts " #{board[0]}  | #{board[1]}  | #{board[2]} "
   puts "-----------"
-  puts " board[3]  | board[4]  | board[5]   "
+  puts " #{board[3]}  | #{board[4]}  | #{board[5]} "
   puts "-----------"
-  puts " board[6]  | board[7]  | board[8]   "
+  puts " #{board[6]}  | #{board[7]}  | #{board[8]}   "
 end
 
       board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
