54ffd53cf0571ebeff98c4523cac0fdc94ad986d
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:53:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -21,7 +21,7 @@ def display_board(board)
   rows = [" #{board[0]} | #{board[1]} | #{board[2]},
           "-----------",
           " #{board[3]} | #{board[4]} | #{board[5]}",
-          "-----------"
+          "-----------",
           " #{board[6]} | #{board[7]} | #{board[8]}"]
   
   
