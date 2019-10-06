1acf2b1d5507456627af731b6f05bebf20b7e068
Mark Kauffman <markkauffman22@gmail.com>
2019-10-06 05:05:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,7 +12,7 @@ end
 def display_board(board) 
   puts " board[0]  | board[1]  | board[2]   "
   puts "-----------"
-  puts "   |   |   "
+  puts " board[0]  | board[1]  | board[2]   "
   puts "-----------"
   puts "   |   |   "
 end
